38136ff3816ad94b6e0e5f381f4903d37cacc5cd
John Nguyen <nguyenduyjohn180@gmail.com>
2023-03-22 19:19:22 -0700

    deleted commented out code and attempted to display images

--- app/src/main/java/com/example/githubsearchwithnavigation/ui/MainPageFragment.kt
+++ app/src/main/java/com/example/githubsearchwithnavigation/ui/MainPageFragment.kt
@@ -1,12 +1,10 @@
 package com.example.githubsearchwithnavigation.ui
 
 import android.app.Application
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.preference.PreferenceManager
@@ -19,7 +17,7 @@ import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.Track
 import android.view.View.OnScrollChangeListener
-import android.widget.ImageButton
+import android.widget.*
 import com.example.githubsearchwithnavigation.data.*
 import kotlinx.coroutines.launch
 
@@ -36,14 +34,11 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
     private lateinit var loadingIndicator: CircularProgressIndicator
     private lateinit var songItemTitle: TextView
     private lateinit var songItemArtist: TextView
+    private lateinit var songItemImage: ImageView
 
     private val clientId = "ef5c3cd0164544e28df769ab91ed8fdb"
     private val redirectUri = "http://com.spotify.android.githubsearchwithnavigation/callback"
     private var spotifyAppRemote: SpotifyAppRemote? = null
-
-    // PLAYLISTS TO SHUFFLE BASED ON USER SETTINGS
-
-    //
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,19 +55,12 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
          */
         songItemTitle = view.findViewById(R.id.tv_song_title)
         songItemArtist = view.findViewById(R.id.tv_song_artist)
+        songItemImage = view.findViewById(R.id.trackImg)
 
         searchResultsListRV = view.findViewById(R.id.rv_search_results)
         searchResultsListRV.layoutManager = LinearLayoutManager(requireContext())
         searchResultsListRV.setHasFixedSize(true)
 //        searchResultsListRV.adapter = repoListAdapter
-
-        /*
-         * Set up an observer on the current search results.  Every time the search results change,
-         * send the new search results into the RecyclerView adapter to be displayed.
-         */
-//        viewModel.searchResults.observe(viewLifecycleOwner) { searchResults ->
-//            repoListAdapter.updatePlaylist(searchResults)
-//        }
 
         /*
          * Set up an observer on the loading status of the API query.  Display the correct UI
@@ -121,6 +109,7 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
 
                     songItemTitle.text = track.name
                     songItemArtist.text = track.artist.name
+                    songItemImage.setImageURI(Uri.parse(track.imageUri.raw))
                 }
             }
         }
@@ -134,6 +123,7 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
 
                     songItemTitle.text = track.name
                     songItemArtist.text = track.artist.name
+                    songItemImage.setImageURI(Uri.parse(track.imageUri.raw))
                 }
             }
         }
@@ -166,28 +156,6 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
         })
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//        val connectionParams = ConnectionParams.Builder(clientId)
-//            .setRedirectUri(redirectUri)
-//            .showAuthView(true)
-//            .build()
-//
-//        SpotifyAppRemote.connect(requireContext(), connectionParams, object : Connector.ConnectionListener {
-//            override fun onConnected(appRemote: SpotifyAppRemote) {
-//                spotifyAppRemote = appRemote
-//                Log.d("MainActivity", "Connected! Yay!")
-//                // Now you can start interacting with App Remote
-//                connected()
-//            }
-//
-//            override fun onFailure(throwable: Throwable) {
-//                Log.e("MainActivity", throwable.message, throwable)
-//                // Something went wrong when attempting to connect! Handle errors here
-//            }
-//        })
-//    }
-
     private fun connected() {
         spotifyAppRemote?.let {
             val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
@@ -199,10 +167,12 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
             // Subscribe to PlayerState
             it.playerApi.subscribeToPlayerState().setEventCallback {
                 val track: Track = it.track
-                Log.d("MainActivity", track.name + " by " + track.artist.name)
+                Log.d("MainActivity", track.name + " by " + track.artist.name + ", imageUri: " + track.imageUri.raw)
 
                 songItemTitle.text = track.name
                 songItemArtist.text = track.artist.name
+                songItemImage.setImageURI(null)
+                songItemImage.setImageURI(Uri.parse(track.imageUri.raw))
             }
         }
     }
@@ -213,56 +183,4 @@ class MainPageFragment: Fragment(R.layout.main_page_fragment) {
             SpotifyAppRemote.disconnect(it)
         }
     }
-
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.activity_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
-                true
-            }
-            R.id.action_bookmarks -> {
-                val intent = Intent(this, LikedSongsFragment::class.java)
-                startActivity(intent)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-
-        val action: Int = MotionEventCompat.getActionMasked(event)
-
-        return when (action) {
-            MotionEvent.ACTION_DOWN -> {
-                Log.d(TAG, "Action was DOWN")
-                true
-            }
-            MotionEvent.ACTION_MOVE -> {
-                Log.d(TAG, "Action was MOVE")
-                true
-            }
-            MotionEvent.ACTION_UP -> {
-                Log.d(TAG, "Action was UP")
-                true
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                Log.d(TAG, "Action was CANCEL")
-                true
-            }
-            MotionEvent.ACTION_OUTSIDE -> {
-                Log.d(TAG, "Movement occurred outside bounds of current screen element")
-                true
-            }
-            else -> super.onTouchEvent(event)
-        }
-    }*/
 }
